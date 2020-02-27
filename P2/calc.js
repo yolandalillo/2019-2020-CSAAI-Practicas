fa90d656718921f188e020c0ed59a0b9d1e98380
yolandalillo <yolandalillom@gmail.com>
2020-02-27 19:57:51 +0100

    añadiendo comentarios al js

--- P2/calc.js
+++ P2/calc.js
@@ -1,27 +1,29 @@
+//Mensaje para comprobar que inicia nuestro js
 console.log("Iniciando calculadora");
-
+//Variables
 display = document.getElementById("display")
 igual  = document.getElementById("igual")
 clear = document.getElementById("clear")
 coma = document.getElementById("coma")
 
-
-
 let digitos = document.getElementsByClassName('cdigito');
 let operadores = document.getElementsByClassName('operador')
 
+//Bucle con array para los digitos del 0 al 9
 for (i=0; i<digitos.length; i++){
   digitos[i].onclick = (ev) =>{
     digito(ev.target);
   }
 }
 
+//Bucle con array para los operadores +,-,*,/,^
 for (i=0; i<operadores.length; i++){
   operadores[i].onclick = (ev) =>{
     digito(ev.target);
   }
 }
 
+//funcion para eliminar cero al principio del display
 function digito(boton) {
 
   if (display.innerHTML == "0") {
