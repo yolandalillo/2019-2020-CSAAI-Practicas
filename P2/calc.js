8d89e039ef4ce7321bd5d7b2a4a28d852fc7e0b7
yolandalillo <yolandalillom@gmail.com>
2020-02-27 13:55:33 +0100

    Editando estilo

---
+++ P2/calc.js
@@ -0,0 +1,47 @@
+console.log("Iniciando calculadora");
+
+display = document.getElementById("display")
+suma = document.getElementById("suma")
+resta = document.getElementById("resta")
+multiplica = document.getElementById("multiplica")
+divide = document.getElementById("divide")
+
+igual  = document.getElementById("igual")
+clear = document.getElementById("clear")
+coma = document.getElementById("coma")
+exp = document.getElementById("exp")
+
+
+let digitos = document.getElementsByClassName('cdigito');
+
+for (i=0; i<digitos.length; i++){
+  digitos[i].onclick = (ev) =>{
+    digito(ev.target);
+  }
+}
+
+function digito(boton) {
+
+  if (display.innerHTML == "0") {
+    display.innerHTML = boton.value;
+  }else {
+  display.innerHTML += boton.value;
+  }
+}
+//Tengo que hacer array para todos los operadores
+
+//-- Insertar simbolo de sumar
+suma.onclick = () => {
+  display.innerHTML += suma.value;
+}
+
+
+//-- Evaluar la expresion
+igual.onclick = () => {
+  display.innerHTML = eval(display.innerHTML);
+}
+
+//-- Poner a cero la expresion
+clear.onclick = () => {
+  display.innerHTML = "0";
+}
