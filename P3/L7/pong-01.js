542b27049934ac24e255e083e3445182eeff0230
yolandalillo <yolandalillom@gmail.com>
2020-03-12 09:31:51 +0100

    Primer ejemplo

---
+++ P3/L7/pong-01.js
@@ -0,0 +1,16 @@
+console.log("Ejecutando JS");
+
+//-- Obtener el objeto canvas
+const canvas = document.getElementById("canvas");
+
+//-- Sus dimensiones las hemos fijado en el fichero
+//-- HTML. Las vemos impresas en la consola
+console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
+
+// para poder dibujar en el canvas
+//const ctx = canvas.getContext("2d");
+
+//esto lo hemos probado en la consola
+//x = 1
+//`x=${x}, x incrementada:${x+1}`
+//poner primero una y luego otra sino no funciona
