e26c454b82ebfa71c597560c036574edd39bf60e
yolandalillo <yolandalillom@gmail.com>
2020-04-15 18:11:24 +0200

    Intentando reproducción automática

--- P4/video.js
+++ P4/video.js
@@ -7,31 +7,27 @@ console.log("Ejecutando JS...");
 
 //--Obtener elemento de vído y configurarlo
 const video1 =  document.getElementById("video1")
-video1.width = 250; //--Tamaño de la pantalla de video
-video1.height = 150;
+video1.width = 200; //--Tamaño de la pantalla de video
+video1.height = 100;
 
 const video2 = document.getElementById("video2");
-video2.width = 250; //--Tamaño de la pantalla de video
-video2.height = 150;
+video2.width = 200; //--Tamaño de la pantalla de video
+video2.height = 100;
 
-//--Imagen estática a mostrar cuando el video no
-//-- ha arrancado
-video1.poster ="https://github.com/yolandalillo/mis_recursos/raw/master/descarga.png";
-video2.poster ="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
-
-
+const video3 = document.getElementById("video3");
+video3.width = 200; //--Tamaño de la pantalla de video
+video3.height = 100;
 //--Obtener los botones
 const play = document.getElementById("play1");
-const stop = document.getElementById("stop1");
 const play2 = document.getElementById("play2");
-const stop2 = document.getElementById("stop2");
+const play3 = document.getElementById("play3");
+
 
 //--Función de retrollamda del botón ver
 play.onclick = () =>{
   console.log("Click!");
   video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
   video1.play();
-  
 };
 
 play2.onclick = () =>{
@@ -39,18 +35,8 @@ play2.onclick = () =>{
   video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
   video2.play();
 };
-
-//--Funcion de retrollamada del boton de parar
-stop.onclick = () => {
-  video1.pause();
-  //--Quitar la fuente de video, para que se muestre la
-  //--imagen definida en el atributo poster
-  video1.src=null;
-}
-
-stop2.onclick = () => {
-  video2.pause();
-  //--Quitar la fuente de video, para que se muestre la
-  //--imagen definida en el atributo poster
-  video2.src=null;
-}
+play3.onclick = () =>{
+  console.log("Click!");
+  video3.src="https://github.com/yolandalillo/mis_recursos/raw/master/video1.mp4"
+  video3.play();
+};
