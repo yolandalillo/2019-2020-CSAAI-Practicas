dc928ef63ae0702b856ddbfb741aa07243770250
yolandalillo <yolandalillom@gmail.com>
2020-04-16 10:03:25 +0200

    Mirando que no haya retardo

--- P4/video.js
+++ P4/video.js
@@ -7,16 +7,20 @@ console.log("Ejecutando JS...");
 
 //--Obtener elemento de vído y configurarlo
 const video1 =  document.getElementById("video1")
-video1.width = 200; //--Tamaño de la pantalla de video
-video1.height = 100;
+video1.width = 300; //--Tamaño de la pantalla de video
+video1.height = 200;
 
 const video2 = document.getElementById("video2");
-video2.width = 200; //--Tamaño de la pantalla de video
-video2.height = 100;
+video2.width = 300; //--Tamaño de la pantalla de video
+video2.height = 200;
 
 const video3 = document.getElementById("video3");
-video3.width = 200; //--Tamaño de la pantalla de video
-video3.height = 100;
+video3.width = 300; //--Tamaño de la pantalla de video
+video3.height = 200;
+
+const video4= document.getElementById("video4");
+video4.width = 600;
+video4.height = 500;
 //--Obtener los botones
 
 video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
@@ -24,3 +28,25 @@ video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
 video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
 
 video3.src="https://github.com/yolandalillo/mis_recursos/raw/master/video1.mp4"
+
+video4.poster ="https://github.com/yolandalillo/mis_recursos/raw/master/descarga.png";
+
+
+play1.onclick = () =>{
+  console.log("Click video 1");
+  video4.src= video1.src;
+  video4.currentTime = video1.currentTime;
+
+};
+play2.onclick = () =>{
+  console.log("Click video 1");
+  video4.src= video2.src;
+  video4.currentTime = video2.currentTime;
+
+};
+play3.onclick = () =>{
+  console.log("Click video 1");
+  video4.src= video3.src;
+  video4.currentTime = video3.currentTime;
+
+};
