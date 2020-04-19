2feb45446d031a7742064aac9d663cd220992a5f
yolandalillo <yolandalillom@gmail.com>
2020-04-19 19:08:56 +0200

    Probando cuarto canal, imagen estática

--- P4/video.js
+++ P4/video.js
@@ -1,6 +1,3 @@
-//--https://giphy.com/explore/la-casa-de-papel
-//--url para descargar gifs utiles para mi practica
-
 //--Mirar S6 donde hay ejemplos de autoreproduccion, video con controles,
 //--control desde js, youtube.
 console.log("Ejecutando JS...");
@@ -23,6 +20,7 @@ video4.width = 820;
 video4.height = 500;
 
 //--Obtener los botones
+imagen.src="https://github.com/yolandalillo/mis_recursos/raw/master/pruebas.jpg"
 
 video4.poster ="https://github.com/yolandalillo/mis_recursos/raw/master/cb.jpg";
 
@@ -38,6 +36,7 @@ play1.onclick = () =>{
   video1.style.border = 'dotted #DA1D23'
   video2.style.border = null;
   video3.style.border = null;
+  imagen.style.border = null;
   video4.src= video1.src;
   video4.muted = false;
   video4.currentTime = video1.currentTime;
@@ -50,6 +49,7 @@ play2.onclick = () =>{
   video2.style.border = 'dotted #DA1D23'
   video1.style.border = null;
   video3.style.border = null;
+  imagen.style.border = null;
   video4.src= video2.src;
   video4.muted = false;
   video4.currentTime = video2.currentTime;
@@ -61,8 +61,18 @@ play3.onclick = () =>{
   video3.style.border = 'dotted #DA1D23'
   video2.style.border = null;
   video1.style.border = null;
+  imagen.style.border = null;
   video4.src= video3.src;
   video4.muted = false;
   video4.currentTime = video3.currentTime;
 
 };
+
+play4.onclick = () =>{
+  video4.poster = imagen.src;
+  imagen.style.border = 'dotted #DA1D23'
+  video3.style.border = null;
+  video2.style.border = null;
+  video1.style.border = null;
+  video4.src = null;
+}
