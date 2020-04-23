f79ac18f66cd8333c712a9759eeb6c20ce5a7c2e
yolandalillo <yolandalillom@gmail.com>
2020-04-23 23:20:32 +0200

    Falta lineal las fotos, colores, diseño y más adicionales.

--- P5/filtro.js
+++ P5/filtro.js
@@ -16,6 +16,8 @@ const range_valueB = document.getElementById('range_valueB');
 const grises = document.getElementById('grises');
 const original = document.getElementById('original');
 const negativo = document.getElementById('negativo');
+const sepia = document.getElementById('sepia');
+
 
 //-- Función de retrollamada de imagen cargada
 
@@ -63,11 +65,30 @@ negativo.onclick = () =>{
     blue_neg = 255 - data[i+2];
 
     data[i] = red_neg;
-    data[i+1] = red_neg;
-    data[i+2] = red_neg;
+    data[i+1] = green_neg;
+    data[i+2] = blue_neg;
   }
   //--Poner la imagen modificada en el canvas
   ctx.putImageData(imgData, 0,0);
+}
+
+sepia.onclick = () => {
+  console.log("Imagen en sepia");
+  var luminosidad = 0;
+  //--Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0,0,canvas.width, canvas.height);
+  //--Obtener el array con todos los píxeles
+  let data = imgData.data;
+  for (let i = 0; i < data.length; i+=4) {
+    //calcula la luminosidad
+    luminosidad = .3 * data[i] + .6 * data[i + 1] + .1 * data[i + 2];
+		data[i] = Math.min(luminosidad + 40, 255); // rojo
+		data[i + 1] = Math.min(luminosidad + 15, 255); // verde
+		data[i + 2] = luminosidad; // azul
+  }
+  //--Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0,0);
+
 }
 
 function colores() {
